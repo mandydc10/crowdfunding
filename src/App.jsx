59c44c521aa555e2edc8cfb4614a2ac4d9c9efc1
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-02-25 11:55:35 +0800

    Added Pledge page/form in

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import HomePage from "./pages/HomePage";
 import CreateUserPage from "./pages/CreateUserPage";
 import LoginPage from "./pages/LoginPage";
 import ProjectPage from "./pages/ProjectPage";
+import PledgePage from "./pages/PledgePage";
 
 // Components
 import Nav from "./components/Nav/Nav";
@@ -33,6 +34,7 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: "/", element: <HomePage /> },
+      { path: "/donate", element: <PledgePage /> },
       { path: "/signup", element: <CreateUserPage /> },
       { path: "/login", element: <LoginPage /> },
       { path: "/project/:id", element: <ProjectPage /> },
---
+++ src/pages/PledgePage.jsx
@@ -0,0 +1,14 @@
+// Components
+import PledgeForm from "../components/PledgeForm/PledgeForm";
+
+function PledgePage() {
+    return (
+        <div className="page-container">
+            <h2>Donate today!</h2>
+            <PledgeForm />
+            <p>test comment</p>
+        </div>
+    )
+}
+
+export default PledgePage;
