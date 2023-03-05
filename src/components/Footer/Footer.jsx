29c0f06fd346e63ad7d14794fdfed4ca76896f15
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-03-05 20:08:35 +0800

    Commented out footer logos for netlify deploy test

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 import './Footer.css';
-import fblogo from '../../../src/images/fblogo.png';
-import iglogo from '../../../src/images/instagram_glyph_white.png';
+// import fblogo from '/src/images/fblogo.png';
+// import iglogo from '/src/images/instagram_glyph_white.png';
 
 function Footer() {
     return (
         <footer>
             <div id="socials">
-                <Link to="/" >
+                {/* <Link to="/" >
                     <img src={fblogo} width="30px" />
                 </Link>
                 <Link to="/" >
                     <img src={iglogo} width="30px" />
-                </Link>
+                </Link> */}
             </div>
             <p>&#169; Communitarian 2023</p>
         </footer >
